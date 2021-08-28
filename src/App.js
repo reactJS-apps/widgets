0cebb32811eb9a7fcea238587ce1b069fd98a9b8
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-08-28 16:34:51 +0500

    make a search request to Wikipedia API with axios

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default function App() {
 
   return (
     <div className="ui container" style={{ marginTop: '5%' }}>
+      <Accordion items={items} />
       <Search />
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Search() {
   const [term, setTerm] = useState('');
+
+  const baseURL = 'https://en.wikipedia.org/w/api.php';
+
+  useEffect(() => {
+    (async () => {
+      await axios.get(`${baseURL}`, {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    })();
+  }, [term]);
 
   return (
     <div>
