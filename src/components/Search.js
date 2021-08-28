70e4ab2f09fff6ffdf3b66b9262bb611e1f24efd
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-08-28 16:45:03 +0500

    make a search request to wikipedia api using async/await syntax inside the useEffect hook and make default search term in the state

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Search() {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('web development');
+  const [results, setResults] = useState([]);
+
+  console.log(results);
 
   const baseURL = 'https://en.wikipedia.org/w/api.php';
 
   useEffect(() => {
-    (async () => {
-      await axios.get(`${baseURL}`, {
+    const search = async () => {
+      const { data } = await axios.get(`${baseURL}`, {
         params: {
           action: 'query',
           list: 'search',
@@ -17,7 +20,10 @@ export default function Search() {
           srsearch: term,
         },
       });
-    })();
+
+      setResults(data.query.search);
+    };
+    if (term) search();
   }, [term]);
 
   return (
