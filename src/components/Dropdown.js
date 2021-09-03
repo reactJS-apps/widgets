815fb3d0cdd3cc190d951b961f3bf53361da1aad
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-09-03 16:45:05 +0500

    make use of useRef hook to get access to current DOM element and close the select element when clicked outside of it

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Dropdown({ options, selected, onSelect }) {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      (e) => {
+        if (ref.current.contains(e.target)) return;
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
+
   const renderedOptions = options.map((option, index) => {
     if (option.value === selected.value) {
       return null;
     }
     return (
-      <div key={index} className="item" onClick={() => onSelect(option)}>
+      <div
+        key={index}
+        className="item"
+        onClick={() => {
+          onSelect(option);
+        }}
+      >
         {option.label}
       </div>
     );
   });
 
   return (
-    <div className="ui form">
+    <div className="ui form" ref={ref}>
       <div className="field">
         <label className="label">Select a Color</label>
         <div
