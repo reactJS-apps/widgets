94ed64fb625a0f13eed8ef52d80af74baf98d1c0
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-09-04 22:04:52 +0500

    detect navigation event by a popStateEvent and listening to it in Route component

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,6 +4,9 @@ export default function Link({ href, className, children }) {
   const onClick = (e) => {
     e.preventDefault();
     window.history.pushState({}, '', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
   return (
     <a href={href} className={className} onClick={onClick}>
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,3 +1,17 @@
+import React, { useEffect } from 'react';
+
 export default function Route({ path, children }) {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log('Location change');
+    };
+
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
+
   return window.location.pathname === path ? children : null;
 }
