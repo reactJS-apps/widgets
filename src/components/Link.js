121a48042a29e3e7fa3bdc14039ef2e0ef00aacb
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-09-04 21:59:54 +0500

    change the URL using window.history.pushState() method

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Link({ href, className, children }) {
   const onClick = (e) => {
     e.preventDefault();
+    window.history.pushState({}, '', href);
   };
   return (
     <a href={href} className={className} onClick={onClick}>
