75a18ab32014fc116c3d983510a1aeff928735cd
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-09-04 22:10:40 +0500

    update Route component to render appropriate Componentswhen URL is changed

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Route({ path, children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
   useEffect(() => {
     const onLocationChange = () => {
-      console.log('Location change');
+      setCurrentPath(window.location.pathname);
     };
 
     window.addEventListener('popstate', onLocationChange);
@@ -13,5 +15,5 @@ export default function Route({ path, children }) {
     };
   }, []);
 
-  return window.location.pathname === path ? children : null;
+  return currentPath === path ? children : null;
 }
